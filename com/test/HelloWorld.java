c1aa05bdc6a0a1770e4d0a5ce225388bc662747a
ZhouXiang <1095971114@qq.com>
2023-01-10 12:07:20 +0800

    First test for git

---
+++ com/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.test;
+
+/**
+ * @projectName: data-structure-test
+ * @package: com.test
+ * @className: HelloWorld
+ * @author: Zhou xiang
+ * @description: test
+ * @date: 2023/1/10 11:25
+ * @version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
