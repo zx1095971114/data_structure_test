627e71ee13edeb688f4587286ab556f6446aa773
ZhouXiang <1095971114@qq.com>
2023-01-10 12:49:22 +0800

    Branch Test

---
+++ com/test/com/test/BranchTest.java
@@ -0,0 +1,16 @@
+package com.test.com.test;
+
+/**
+ * @projectName: data-structure-test
+ * @package: com.test.com.test
+ * @className: BranchTest
+ * @author: Zhou xiang
+ * @description: 分支测试
+ * @date: 2023/1/10 12:44
+ * @version: 1.0
+ */
+public class BranchTest {
+    public static void main(String[] args){
+        System.out.println("Branch Test");
+    }
+}
